a8c36e3c49c1116532cbca7857fa40b0ff702c7d
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-10 19:17:28 +0530

    admin signup structure added

--- admin/signup.php
+++ admin/signup.php
@@ -11,20 +11,53 @@
 	<!-- Adding reference to font awesome -->
 	<link rel="stylesheet" href="assets/vendor/font/fontawesome-all.css">
 	<!-- Default style-sheet is for 'media' type screen (color computer display).  -->
-	<link rel="stylesheet" media="screen" href="assets/css/style.css">
+	<link rel="stylesheet" media="screen" href="../assets/css/style.css">
 </head>
 <body>
 	<div class="container">
 		<!-- Start of the header -->
 		<header>
+			<?php
+			include '../partials/admin-header.php';
+			?>
 		</header>
 		<!-- end of header -->
 		<!-- Start of main -->
 		<main>
+			<!-- admin signup form start here-->
+			<div class="main-content">
+				<div class="wrapper">
+						<div class="register-form">
+							<h2 class="capitalize">please sign up</h2>
+							<form action="<?php echo htmlentities('../includes/signup.inc.php'); ?>" method="post">
+								<div class="form-input">
+									<input type="text" name="name" placeholder="Name">
+									<label class="form-error"></label>
+								</div>
+								<div class="form-input">
+									<input type="text" name="username" placeholder="Username">
+									<label class="form-error"></label>
+								</div>
+								<div class="form-input">
+									<input type="password" name="password" placeholder="Password">
+									<label class="form-error"></label>
+								</div>
+								<div class="form-button">
+									 <input type="submit" class="capitalize transition" name="signup" value="sign up">
+								</div>
+								<label class="form-error"></label>
+							</form>
+						</div>
+				</div>
+			</div>
+			<!-- admin signup form ends here -->
 		</main>
 		<!-- End of main -->
 		<!-- start of footer -->
 		<footer>
+			<?php
+			include '../partials/admin-footer.php';
+			?>
 		</footer>
 		<!-- end of footer -->
 	</div>
