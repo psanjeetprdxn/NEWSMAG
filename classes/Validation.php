11b8b4a08fddc5ada992ec0034e39cb345479085
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-10 20:18:01 +0530

    added function to validate username

--- classes/Validation.php
+++ classes/Validation.php
@@ -18,4 +18,18 @@ class Validation extends Connection {
         $data = htmlentities($data);
         return $data;
     }
+
+    /*
+    ****************************************
+       CHECKS IF USERNAME IS VALID
+    ****************************************
+    */
+    public function validateUsername($username)
+    {
+        $isValid = false;
+        if (!empty(preg_match('/^[A-Za-z][A-Za-z0-9]{5,31}$/', $this->sanitize($username))) {
+            $isValid = true;
+        }
+        return $isValid;
+    }
 }
