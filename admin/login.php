eeb79eef8b60f14221cee20a718d4972474f883d
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-10 18:57:16 +0530

    admin login structure added

--- admin/login.php
+++ admin/login.php
@@ -11,20 +11,50 @@
 	<!-- Adding reference to font awesome -->
 	<link rel="stylesheet" href="assets/vendor/font/fontawesome-all.css">
 	<!-- Default style-sheet is for 'media' type screen (color computer display).  -->
-	<link rel="stylesheet" media="screen" href="assets/css/style.css">
+	<link rel="stylesheet" media="screen" href="../assets/css/style.css">
 </head>
 <body>
 	<div class="container">
 		<!-- Start of the header -->
 		<header>
+			<?php
+			include '../partials/admin-header.php';
+			?>
 		</header>
 		<!-- end of header -->
 		<!-- Start of main -->
 		<main>
+			<div class="main-content">
+				<div class="wrapper">
+						<div class="register-form">
+							<h2 class="capitalize">please sign in</h2>
+							<form action="<?php echo htmlentities('../includes/login.inc.php'); ?>" method="post">
+								<div class="form-input">
+									<input type="text" name="username" placeholder="Username">
+									<label class="form-error"></label>
+								</div>
+								<div class="form-input">
+									<input type="password" name="password" placeholder="Password">
+									<label class="form-error"></label>
+								</div>
+								<div class="form-button">
+									<a href="#FIXME" class="capitalize transition" title="Forgot Password">forgot password</a>
+								</div>
+								<div class="form-button">
+									 <input type="submit" class="capitalize transition" name="login" value="login">
+								</div>
+								<label class="form-error"></label>
+							</form>
+						</div>
+				</div>
+			</div>
 		</main>
 		<!-- End of main -->
 		<!-- start of footer -->
 		<footer>
+			<?php
+			include '../partials/admin-footer.php';
+			?>
 		</footer>
 		<!-- end of footer -->
 	</div>
