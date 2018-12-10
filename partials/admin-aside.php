fa9070504acb7fae9e9a4cda0d30d0193857c4a1
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-10 16:15:45 +0530

    added admin-aside structure

---
+++ partials/admin-aside.php
@@ -0,0 +1,11 @@
+<h2 class="uppercase">adage</h2>
+<nav>
+  <ul>
+    <li>
+      <a href="add.php" class="uppercase transition" title="Add Article">add article</a>
+    </li>
+    <li>
+      <a href="browse.php" class="uppercase transition" title="Browse Article">browse article</a>
+    </li>
+  </ul>
+</nav>
