c53da376276c5f1c7139ff17488db3118cd48f72
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-10 16:02:10 +0530

    admin-header structure added

---
+++ partials/admin-header.php
@@ -0,0 +1,19 @@
+<div class="wrapper">
+  <div class="admin-header-left">
+    <h1>Ad Age Group | CMS</h1>
+  </div>
+  <ul>
+    <li>
+      <a href="signup.php" class="transition" title="Sign Up">Sign Up</a>
+    </li>
+    <li>
+      <a href="login.php" class="transition" title="Login">Login</a>
+    </li>
+    <li>
+      <a href="#FIXME" class="profile-name transition" title="Sign Up">Libby Swan</a>
+    </li>
+    <li>
+      <a href="logout.inc.php" class="transition" title="Logout">Logout</a>
+    </li>
+  </ul>
+</div>
