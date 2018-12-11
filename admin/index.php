b247d8fac27c2d50a586b68621b54502e07a3c14
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-11 19:05:44 +0530

    admin index page structure added

--- admin/index.php
+++ admin/index.php
@@ -9,22 +9,52 @@
 	<!-- Place favicon.ico in the root directory: mathiasbynens.be/notes/touch-icons -->
 	<link rel="shortcut icon" href="favicon.ico" />
 	<!-- Adding reference to font awesome -->
-	<link rel="stylesheet" href="assets/vendor/font/fontawesome-all.css">
+	<link rel="stylesheet" href="../assets/vendor/font/fontawesome-all.css">
 	<!-- Default style-sheet is for 'media' type screen (color computer display).  -->
-	<link rel="stylesheet" media="screen" href="assets/css/style.css">
+	<link rel="stylesheet" media="screen" href="../assets/css/style.css">
 </head>
 <body>
+  <?php
+  if (!isset($_SESSION)) {
+      session_start();
+  }
+  // checks if session exists
+  if (!isset($_SESSION['user_id'])) {
+      header ("Location: login.php?msg=notLogin");
+      exit();
+  }
+  ?>
 	<div class="container">
 		<!-- Start of the header -->
 		<header>
+      <?php
+			include '../partials/admin-header.php';
+			?>
 		</header>
 		<!-- end of header -->
 		<!-- Start of main -->
 		<main>
+      <div class="main-content">
+        <div class="wrapper">
+          <aside>
+            <?php
+            include '../partials/admin-aside.php';
+            ?>
+          </aside>
+          <div class="redesign">
+            <h2>What's New For the Redesign</h2>
+            <p>Hey <span class="admin-name"><?php echo $user->getNameById($_SESSION['user_id']); ?></span>, the CMS has undergone a few small changes
+              for the Ad Age and creativity redesigns. Below we'll walk you through them.</p>
+          </div><!--end of redesign -->
+        </div>
+      </div>
 		</main>
 		<!-- End of main -->
 		<!-- start of footer -->
 		<footer>
+      <?php
+			include '../partials/admin-footer.php';
+			?>
 		</footer>
 		<!-- end of footer -->
 	</div>
