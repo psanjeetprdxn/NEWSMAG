dd43c4aed2cd6f503b0bca340b4828730e28650d
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-11 20:14:34 +0530

    added functionality to display articles dynamically in browse section

--- admin/browse.php
+++ admin/browse.php
@@ -14,9 +14,22 @@
 	<link rel="stylesheet" media="screen" href="../assets/css/style.css">
 </head>
 <body>
+  <?php
+  if (!isset($_SESSION)) {
+      session_start();
+  }
+  // checks if session exists
+  if (!isset($_SESSION['user_id'])) {
+    	header ("Location: login.php?msg=notLogin");
+    	exit();
+  }
+  ?>
 	<div class="container">
 		<!-- Start of the header -->
 		<header>
+      <?php
+			include '../partials/admin-header.php';
+			?>
 		</header>
 		<!-- end of header -->
 		<!-- Start of main -->
@@ -24,10 +37,13 @@
       <div class="main-content">
         <div class="wrapper">
           <aside>
+            <?php
+            include '../partials/admin-aside.php';
+            ?>
           </aside>
           <div class="all-articles">
             <div class="search-form">
-              <form action="#FIXME" method="GET">
+              <form action="<?php $_SERVER['PHP_SELF']; ?>" method="GET">
                 <select name="section">
                   <option class="capitalize" selected disabled>Section</option>
                   <option value="politics" class="capitalize">politics</option>
@@ -37,6 +53,16 @@
                 <input type="submit" class="transition capitalize" name="Search" value="Search">
               </form>
             </div>
+            <?php
+            // to display articles
+            $article = new Article;
+            if (isset($_GET['section'])) {
+                $articles = $article->displayBySection($_GET['section']);
+            } else {
+                $articles = $article->display();
+            }
+            if (!empty($articles)) {
+            ?>
             <div class="articles-details">
               <!-- ARTICLE COLUMN NAME -->
               <div class="table-row-header">
@@ -49,17 +75,29 @@
               </div>
               <!-- end of ARTICLE column name -->
               <!-- ARTICLE DATA -->
+              <?php
+              foreach ($articles as $art) {
+              ?>
               <div class="table-row-data">
-                <div class="table-data"><span></span></div>
-                <div class="table-data update"><a class="edit" href="#FIXME" title="Update">Update</a></div>
-                <div class="table-data"><span></span></div>
-                <div class="table-data"><span></span></div>
-                <div class="table-data"><span></span></div>
-                <div class="table-data"><a class="delete" href="#FIXME" title="Delete">Delete</a></div>
+                <div class="table-data"><span><?php echo $art['article_id']; ?></span></div>
+                <div class="table-data update"><a class="edit" href="update.php?article_id=<?php echo $art['article_id']; ?>" title="Update">Update</a></div>
+                <div class="table-data"><span><?php echo $art['title']; ?></span></div>
+                <div class="table-data"><span><?php echo $art['section_name']; ?></span></div>
+                <div class="table-data"><span><?php echo date('d/m/Y', strtotime($art['date'])); ?></span></div>
+                <div class="table-data"><a class="delete" href="../includes/delete.inc.php?article_id=<?php echo $art['article_id']; ?>" title="Delete">Delete</a></div>
               </div>
+                <?php
+                }//end of foreach loop
+                ?>
                 <!-- end of article data -->
             </div><!-- end of article details -->
+            <?php
+            } else {
+            ?>
             <span class="article-message">No data to show</span>
+            <?php
+            }//end of else
+            ?>
           </div>
         </div>
       </div>
@@ -67,6 +105,9 @@
 		<!-- End of main -->
 		<!-- start of footer -->
 		<footer>
+      <?php
+			include '../partials/admin-footer.php';
+			?>
 		</footer>
 		<!-- end of footer -->
 	</div>
