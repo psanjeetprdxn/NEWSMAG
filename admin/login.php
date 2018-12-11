02c29b1f0375509c3c2e0c497f3dd84a61fb6911
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-11 17:47:33 +0530

    added validation error message to admin login form

--- admin/login.php
+++ admin/login.php
@@ -9,11 +9,21 @@
 	<!-- Place favicon.ico in the root directory: mathiasbynens.be/notes/touch-icons -->
 	<link rel="shortcut icon" href="favicon.ico" />
 	<!-- Adding reference to font awesome -->
-	<link rel="stylesheet" href="assets/vendor/font/fontawesome-all.css">
+	<link rel="stylesheet" href="../assets/vendor/font/fontawesome-all.css">
 	<!-- Default style-sheet is for 'media' type screen (color computer display).  -->
 	<link rel="stylesheet" media="screen" href="../assets/css/style.css">
 </head>
 <body>
+	<!-- IF SESSION EXISTS THAN REDIRECT TO INDEX PAGE -->
+	<?php
+	if (!isset($_SESSION)) {
+      session_start();
+  }
+	if (isset($_SESSION['user_id'])) {
+			header("Location: index.php");
+			exit();
+	}
+	?>
 	<div class="container">
 		<!-- Start of the header -->
 		<header>
@@ -24,30 +34,65 @@
 		<!-- end of header -->
 		<!-- Start of main -->
 		<main>
+			<!-- admin login form starts here -->
 			<div class="main-content">
 				<div class="wrapper">
-						<div class="register-form">
-							<h2 class="capitalize">please sign in</h2>
-							<form action="<?php echo htmlentities('../includes/login.inc.php'); ?>" method="post">
-								<div class="form-input">
-									<input type="text" name="username" placeholder="Username">
-									<label class="form-error"></label>
-								</div>
-								<div class="form-input">
-									<input type="password" name="password" placeholder="Password">
-									<label class="form-error"></label>
-								</div>
-								<div class="form-button">
-									<a href="#FIXME" class="capitalize transition" title="Forgot Password">forgot password</a>
-								</div>
-								<div class="form-button">
-									 <input type="submit" class="capitalize transition" name="login" value="login">
-								</div>
-								<label class="form-error"></label>
-							</form>
-						</div>
+					<div class="register-form">
+						<h2 class="capitalize">please sign in</h2>
+						<form action="<?php echo htmlentities('../includes/login.inc.php'); ?>" method="post">
+							<div class="form-input">
+								<input type="text" name="username" placeholder="Username">
+								<label class="form-error">
+                  <?php
+                  if (isset($_GET['msg'])) {
+                      if ($_GET['msg'] == 'usernameInvalid') {
+                          echo 'Username must contain letters and numbers from 6-32 characters and must start with letters';
+                      }
+                      if ($_GET['msg'] == 'usernameNotSet') {
+                          echo 'Username attribute is not set';
+                      }
+                  }
+                  ?>
+                </label>
+							</div>
+							<div class="form-input">
+								<input type="password" name="password" placeholder="Password">
+								<label class="form-error">
+                  <?php
+                  if (isset($_GET['msg'])) {
+                      if ($_GET['msg'] == 'passwordInvalid') {
+                          echo 'Password required';
+                      }
+                      if ($_GET['msg'] == 'passwordNotSet') {
+                          echo 'Password attribute is not set';
+                      }
+                  }
+                  ?>
+                </label>
+							</div>
+							<div class="form-button">
+								<a href="#FIXME" class="capitalize transition" title="Forgot Password">forgot password</a>
+							</div>
+							<div class="form-button">
+								 <input type="submit" class="capitalize transition" name="login" value="login">
+							</div>
+							<label class="form-error">
+                <?php
+                if (isset($_GET['msg'])) {
+                  	if ($_GET['msg'] == 'loginFail') {
+                    	echo 'Username or password is incorrect';
+                  	}
+                  	if ($_GET['msg'] == 'notLogin') {
+                    	echo 'You must login to use that page';
+                  	}
+                }
+                ?>
+              </label>
+						</form>
+					</div>
 				</div>
 			</div>
+			<!-- admin login form ends here -->
 		</main>
 		<!-- End of main -->
 		<!-- start of footer -->
