200272be764bf0de13926db0a555a20f29ba6e7f
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-11 19:51:41 +0530

    added functionality to display article details in update form of that particular id

--- admin/update.php
+++ admin/update.php
@@ -14,22 +14,46 @@
 	<link rel="stylesheet" media="screen" href="../assets/css/style.css">
 </head>
 <body>
+  <?php
+  if (!isset($_SESSION)) {
+      session_start();
+  }
+  // checks if session exists
+  if (!isset($_SESSION['user_id'])) {
+    	header ("Location: login.php?msg=notLogin");
+    	exit();
+  }
   ?>
 	<div class="container">
 		<!-- Start of the header -->
 		<header>
+      <?php
+			include '../partials/admin-header.php';
+			?>
 		</header>
 		<!-- end of header -->
 		<!-- Start of main -->
 		<main>
+      <?php
+      $article = new Article();
+      if (isset($_GET['article_id'])) {
+        	$articles = $article->getById($_GET['article_id']);
+      } else {
+        	header("Location: browse.php");
+        	exit();
+      }
+      ?>
       <div class="main-content">
         <div class="wrapper">
           <aside>
+            <?php
+            include '../partials/admin-aside.php';
+            ?>
           </aside>
           <div class="add-article">
             <ul class="cf">
               <li>
-                <a href="#FIXME" class="capitalize active" title="Add Article">edit</a>
+                <a href="update.php?article_id=<?php echo $_GET['article_id']; ?>" class="capitalize active" title="Add Article">edit</a>
               </li>
               <li>
                 <a href="#FIXME" class="capitalize" title="Advance">advance</a>
@@ -39,13 +63,13 @@
               </li>
             </ul>
             <div class="article-form">
-              <form action="#FIXME" method="post" enctype="multipart/form-data">
+              <form action="<?php echo htmlentities('../includes/update.inc.php?article_id='.$_GET['article_id']) ?>" method="post" enctype="multipart/form-data">
                 <div class="fields">
                   <div class="input-label">
                     <label for="title" class="capitalize">title<span class="manditory">*</span></label>
                   </div>
                   <div class="input-fields">
-                    <input type="text" id="title" name="title" value="" required>
+                    <input type="text" id="title" name="title" value="<?php echo $articles['title']; ?>" required>
                     <label class="form-error"></label>
                   </div>
                 </div>
@@ -54,7 +78,7 @@
                     <label for="description" class="capitalize">description<span class="manditory">*</span></label>
                   </div>
                   <div class="input-fields">
-                    <textarea name="description" id="description" required></textarea>
+                    <textarea name="description" id="description" required><?php echo $articles['description']; ?></textarea>
                   </div>
                 </div>
                 <div class="fields">
@@ -64,9 +88,9 @@
                   <div class="input-fields">
                     <select name="section" id="section">
                       <option value="" class="capitalize" disabled>section</option>
-                      <option value="politics" class="capitalize">politics</option>
-                      <option value="tech" class="capitalize">tech</option>
-                      <option value="world" class="capitalize">>world</option>
+                      <option value="politics" class="capitalize" <?php if ($articles['section'] == 'politics') {echo 'selected'; } ?>>politics</option>
+                      <option value="tech" class="capitalize" <?php if ($articles['section'] == 'tech') {echo 'selected'; } ?>>tech</option>
+                      <option value="world" class="capitalize" <?php if ($articles['section'] == 'world') {echo 'selected'; } ?>>world</option>
                     </select>
                   </div>
                 </div>
@@ -75,7 +99,7 @@
                     <label for="thumbnail" class="capitalize">Thumbnail<span class="manditory">*</span></label>
                   </div>
                   <div class="input-fields">
-                    <img src="#FIXME" alt="#FIXME">
+                    <img src="<?php echo 'data:image/jpeg;base64,'.base64_encode($articles['thumbnail_image']); ?>" alt="<?php echo $articles['title']; ?>">
                     <input type="file" name="thumbnail" id="thumbnail">
                   </div>
                 </div>
@@ -93,6 +117,9 @@
 		<!-- End of main -->
 		<!-- start of footer -->
 		<footer>
+      <?php
+			include '../partials/admin-footer.php';
+			?>
 		</footer>
 		<!-- end of footer -->
 	</div>
