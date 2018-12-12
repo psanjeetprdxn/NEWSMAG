4bde5a5502f8f7c352623c879544e0f1a25ed6f0
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-12 14:57:04 +0530

    main page (landing) header strucutre added

--- index.php
+++ index.php
@@ -11,22 +11,90 @@
 	<!-- Adding reference to font awesome -->
 	<link rel="stylesheet" href="assets/vendor/font/fontawesome-all.css">
 	<!-- Default style-sheet is for 'media' type screen (color computer display).  -->
-	<link rel="stylesheet" media="screen" href="assets/css/style.css">
+	<link rel="stylesheet" media="screen" href="assets/css/master.css">
 </head>
 <body>
 	<div class="container">
 		<!-- Start of the header -->
 		<header>
+      <div class="header-top">
+        <div class="wrapper cf">
+          <ul class="social">
+            <li>
+              <a href="#FIXME" class="transition" target="_blank" rel="nofollow" title="Facebook">facebook</a>
+            </li>
+            <li>
+              <a href="#FIXME" class="transition" target="_blank" rel="nofollow" title="Instagram">instagram</a>
+            </li>
+            <li>
+              <a href="#FIXME" class="transition" target="_blank" rel="nofollow" title="Twitter">twitter</a>
+            </li>
+            <li>
+              <a href="#FIXME" class="transition" target="_blank" rel="nofollow" title="Pintrest">pintrest</a>
+            </li>
+            <li>
+              <a href="#FIXME" class="transition" target="_blank" rel="nofollow" title="Linkedin">linkedin</a>
+            </li>
+            <li>
+              <a href="#FIXME" class="transition" target="_blank" rel="nofollow" title="Youtube">youtube</a>
+            </li>
+          </ul>
+        </div>
+      </div><!-- end of header-top -->
+      <div class="header-mid">
+        <div class="wrapper cf">
+          <h1>
+            <a href="#FIXME" title="NewsMag">
+              <img src="https://via.placeholder.com/117x38" alt="NewsMag">
+            </a>
+          </h1>
+          <div class="ad">
+            <a href="#FIXME" title="Buy Now">
+              <img src="https://via.placeholder.com/486x61" alt="Buy Now">
+            </a>
+          </div>
+        </div>
+      </div> <!-- end of header-mid-->
+      <div class="header-bot">
+        <div class="wrapper">
+          <nav>
+            <ul>
+              <li>
+                <a href="#FIXME" class="uppercase transition" title="Home">home</a>
+              </li>
+              <li>
+                <a href="#FIXME" class="uppercase transition" title="blog">blog</a>
+                <ul class="drop-list">
+                  <li>
+                    <a href="#FIXME" class="uppercase transition" title="Dummy">Dummy</a>
+                  </li>
+                  <li>
+                    <a href="#FIXME" class="uppercase transition" title="Dummy">Dummy</a>
+                  </li>
+                  <li>
+                    <a href="#FIXME" class="uppercase transition" title="Dummy">Dummy</a>
+                  </li>
+                  <li>
+                    <a href="#FIXME" class="uppercase transition" title="Dummy">Dummy</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#FIXME" class="uppercase transition" title="Featured">Featured</a>
+              </li>
+              <li class="search">
+                <a href="#FIXME" class="uppercase transition" title="Search">search</a>
+              </li>
+            </ul>
+          </nav>
+        </div><!-- end of header bottom -->
+      </div>
 		</header>
 		<!-- end of header -->
 		<!-- Start of main -->
 		<main>
 		</main>
 		<!-- End of main -->
-		<!-- start of footer -->
-		<footer>
-		</footer>
-		<!-- end of footer -->
 	</div>
 </body>
 </html>
